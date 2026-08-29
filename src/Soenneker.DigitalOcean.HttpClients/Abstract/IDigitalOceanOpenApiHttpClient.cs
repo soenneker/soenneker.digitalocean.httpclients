f75dcf9e7e68729e8ec8407b32a8c084987d4d4b
soenneker <jake@soenneker.com>
2026-08-29 14:16:59 -0500

    Improve public API XML documentation

--- src/Soenneker.DigitalOcean.HttpClients/Abstract/IDigitalOceanOpenApiHttpClient.cs
+++ src/Soenneker.DigitalOcean.HttpClients/Abstract/IDigitalOceanOpenApiHttpClient.cs
@@ -2,13 +2,16 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Threading;
-
 namespace Soenneker.DigitalOcean.HttpClients.Abstract;
-
 /// <summary>
 /// A .NET thread-safe singleton HttpClient for 
 /// </summary>
 public interface IDigitalOceanOpenApiHttpClient: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Returns the configured HTTP client used by the Digital Ocean OpenAPI HTTP Client.
+    /// </summary>
+    /// <param name="cancellationToken">Token used to cancel the operation.</param>
+    /// <returns>A task whose result is the requested HTTP client.</returns>
     ValueTask<HttpClient> Get(CancellationToken cancellationToken = default);
 }
--- src/Soenneker.DigitalOcean.HttpClients/Registrars/DigitalOceanOpenApiHttpClientRegistrar.cs
+++ src/Soenneker.DigitalOcean.HttpClients/Registrars/DigitalOceanOpenApiHttpClientRegistrar.cs
@@ -13,6 +13,8 @@ public static class DigitalOceanOpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="DigitalOceanOpenApiHttpClient"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddDigitalOceanOpenApiHttpClientAsSingleton(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
@@ -24,6 +26,8 @@ public static class DigitalOceanOpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="DigitalOceanOpenApiHttpClient"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddDigitalOceanOpenApiHttpClientAsScoped(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
